c897b7bb01cc19a06b170075eb03d25b8ff5ffaa
SonjaDraijer <sonjadraijer@hotmail.com>
2023-02-23 11:37:43 +0100

    oefening 9 commit

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
+    'jokes.apps.JokesConfig',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
